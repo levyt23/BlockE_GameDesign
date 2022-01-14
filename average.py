5aec9ef5b54d3d04971d3767b14ed290d2dcf373
Taylor <levyt23@greenhill.org>
2022-01-14 09:56:09 -0600

    Todays class 1/14

---
+++ average.py
@@ -0,0 +1,28 @@
+import os
+# Taylor I Levy
+# Jan 14, 2022
+# declare variables, print variables, print types of data, 
+# learn some operators
+
+# this symbol is for comments, means the computer will ignore
+# i want to clear my terminal
+os.system('cls')
+# program is average of three tests
+
+#declare and assign values
+test1=89
+test2=78.5
+test3=86
+Flag=False
+
+#to display things on the screen we use print
+# print(type(test1), type(test2), type(Flag))
+
+#declare Sum to add tests symbol for addition is +
+Sum= test1 + test2 + test3
+#average we will devision ....
+Average= Sum/3
+
+Print('TheSum is',Sum)
+
+Print('The average of 3 tests is', Average)
